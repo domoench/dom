9fa193c0996e2523a6444255dcaea0a01e3fe2d3
David Moench <domoench@gmail.com>
2017-05-25 15:01:14 -0400

    port remaining pages.

--- dom/dom.py
+++ dom/dom.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
 application = Flask(__name__)
 
-@application.route("/")
+@application.route('/')
 def index():
     return render_template('index.html')
+
+@application.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@application.route('/fmri-viz')
+def fmri():
+    return render_template('fmri-viz.html')
+
+@application.route('/music')
+def music():
+    return render_template('music.html')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
